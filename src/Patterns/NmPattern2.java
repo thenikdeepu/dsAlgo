b083dcc1c287ee1337d2c7c79438503d35cbb30d
thenikdeepu <thenikdeepu@gmail.com>
2023-07-23 12:54:40 +0530

    pattern nymber question

--- src/Patterns/NmPattern2.java
+++ src/Patterns/NmPattern2.java
@@ -9,31 +9,40 @@ public class NmPattern2 {
 		int n = s.nextInt();
 		int star = 1;
 		int row = 1;
-		int space = n/2;
+		int space = n-1;
 		int number = 1;
 		numberPattern(n, row, star, space, number);
 
 	}
 
 	private static void numberPattern(int n, int row, int star, int space, int number) {
-		while (row <= n) {
+		while (row <=n*2-1) {
 			int i = 1;
 			while (i <= space) {
-				System.out.print("  ");
+				System.out.print("\t");
 				i++;
 			}
 			int j = 1;
+			int p = number;
 			while (j <= star) {
-				System.out.print("* ");
+				System.out.print(p + "\t");
+
+				if (j <= star / 2) {
+					p++;
+				} else {
+					p--;
+				}
 				j++;
 			}
 
 			if (row <= n / 2) {
 				star += 2;
 				space--;
+				number++;
 			} else {
 				star -= 2;
 				space++;
+				number--;
 			}
 			System.out.println();
 			row++;
